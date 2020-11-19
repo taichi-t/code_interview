b821bd5fcdf13fce7f589f4d631cf7f514e7a228
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-11-18 17:14:39 -0800

    :wrench: Update package.json file.

--- solutions/first_reverse.ts
+++ solutions/first_reverse.ts
@@ -1,10 +1,12 @@
+const str = 'coderbyte';
+
 const FirstReverse = (str: string) => {
   // code goes here
   return str;
 };
 
 // keep this function call here
-console.log(FirstReverse(readline()));
+console.log(FirstReverse(str));
 
 // Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order.
 //  For example: if the input string is "Hello World and Coders"
