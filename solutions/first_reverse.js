5e49d1cbacb28d419456bc2f597daf71e8908726
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-11-19 15:02:40 -0800

    :sparkles: Add first_reverse.js

--- solutions/first_reverse.js
+++ solutions/first_reverse.js
@@ -1,12 +1,13 @@
 const str = 'coderbyte';
+const str2 = "I Love Code"
 
-const FirstReverse = (str: string) => {
-  // code goes here
-  return str;
+const FirstReverse = (str) => {
+  const result = str.split("").reverse().join(" ")
+  return result;
 };
 
 // keep this function call here
-console.log(FirstReverse(str));
+console.log(FirstReverse(str2));
 
 // Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order.
 //  For example: if the input string is "Hello World and Coders"
