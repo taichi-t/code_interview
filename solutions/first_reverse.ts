db6f1110613603c772805108872410ea5afff239
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-11-18 17:06:21 -0800

    :tada: Initial commit

---
+++ solutions/first_reverse.ts
@@ -0,0 +1,11 @@
+const FirstReverse = (str: string) => {
+  // code goes here
+  return str;
+};
+
+// keep this function call here
+console.log(FirstReverse(readline()));
+
+// Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order.
+//  For example: if the input string is "Hello World and Coders"
+//  then your program should return the string sredoC dna dlroW olleH.
